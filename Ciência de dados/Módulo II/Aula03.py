b6a72e246d82ea729e29779602cf1fec7e7cb076
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-09-13 19:56:17 -0300

    Create Aula03.py

---
+++ Ciência de dados/Módulo II/Aula03.py
@@ -0,0 +1,12 @@
+#Variáveis e constantes
+age = 29
+name= 'Caroline'
+print(f'Meu nome é {name} e tenho {age} ano(s) de idade.')
+
+#Outra forma de escrever 
+# age,name =(29,'Caroline')
+# print(f' Meu nome é {name} e eu tenho {age} ano(s) de idade.)
+
+BRAZILIAN_STATES = ("SP", "RJ","SC","RS","MG")
+
+print(BRAZILIAN_STATES)
