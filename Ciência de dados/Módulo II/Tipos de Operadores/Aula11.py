79255fa0b4c7f1c60d27cae7fd4baa69e09cf102
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-09-14 02:33:19 -0300

    Create Aula11.py

---
+++ Ciência de dados/Módulo II/Tipos de Operadores/Aula11.py
@@ -0,0 +1,11 @@
+# Operadores de associação
+frutas = ["limão", "uva", "melão", "morango"]
+curso = "curso de python"
+
+print("laranja" not in frutas)
+print("limão" in frutas)
+print("Python" in curso)
+print("melão" in curso)
+print("curso de python" in curso)
+print("morango" not in frutas)
+print("morango" in frutas)
