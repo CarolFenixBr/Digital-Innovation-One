98411b12f9232fea8327bab34c73acbb082110e9
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-09-15 00:07:39 -0300

    Create Aula17.py

---
+++ Ciência de dados/Módulo II/Estruturas Condicionais e de Repetição em Python/Aula17.py
@@ -0,0 +1,10 @@
+#Estrutura de repetição: FOR ELSE
+texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    if letra.upper()in VOGAIS:
+        print(letra, end="")
+else:
+    print() # adiciona uma quebra de linha
+    print("Executa no final do laço")
