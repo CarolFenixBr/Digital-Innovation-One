7b15844c84e40ef98747b2aea5e46aa92d956fab
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-09-15 00:12:25 -0300

    Create Aula18.py

---
+++ Ciência de dados/Módulo II/Estruturas Condicionais e de Repetição em Python/Aula18.py
@@ -0,0 +1,9 @@
+#Estrutura de repetição: Range com for
+for numero in range(0, 11):
+    print(numero, end=" ")
+
+print() #quebra de linha
+
+#exibindo a tabuada de 5
+for numero in range(0, 51, 5):
+    print(numero, end=" ")
