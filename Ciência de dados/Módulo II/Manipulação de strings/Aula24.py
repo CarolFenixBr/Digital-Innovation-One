6fce469238596ced99ed1ef63b8ac0ce66a6c36f
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-09-26 00:37:56 -0300

    Create Aula24.py

---
+++ Ciência de dados/Módulo II/Manipulação de strings/Aula24.py
@@ -0,0 +1,26 @@
+"""
+String múltiplas linhas
+"""
+nome = "Caroline"
+
+mensagem = f"""
+    Olá meu nome é {nome}
+Eu estou aprendendo Python
+    Essa mensagem tem diferentes recuos.
+    """
+print(mensagem)
+
+print(
+    """
+    ==========Menu==========
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+
+    ========================
+
+            Obrigado por usar nosso sistema, tenha um bom dia!
+    """
+)
