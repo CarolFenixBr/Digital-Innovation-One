4397819b34b27d6dca939f075bcf98812b220bad
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-09-26 00:30:44 -0300

    Create Aula23.py

---
+++ Ciência de dados/Módulo II/Manipulação de strings/Aula23.py
@@ -0,0 +1,12 @@
+"""Fatiamento de string"""
+
+nome = "Caroline Vargas da Silva Costa"
+
+print (nome[0])
+print(nome[-1])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:17:2])
+print(nome[:])
+print(nome[::-1])
