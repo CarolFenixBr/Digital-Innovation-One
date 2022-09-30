65768bc871db09338b00e39f1f63eb97ca586bf3
Caroline <89542446+CarolFenixBr@users.noreply.github.com>
2022-09-30 19:30:12 -0300

    Create Aula25.py

---
+++ Ciência de dados/Módulo II/Estruturas de dados/listas/Aula25.py
@@ -0,0 +1,17 @@
+"""declaração de listas
+"""
+
+frutas = ["laranja", "morango", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2022, 2900, "Niterói", True]
+print(carro)
